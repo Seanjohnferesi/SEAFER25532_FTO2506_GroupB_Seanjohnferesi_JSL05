9fea26f45bc58f1cb1b52e360afc20018c548647
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-09-19 22:31:46 +0200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,87 @@
+ const initialTasks = [
+  {
+    id: 1,
+    title: "Launch Epic Career 🚀",
+    description: "Create a killer Resume",
+    status: "todo",
+  },
+  {
+    id: 2,
+    title: "Master JavaScript 💛",
+    description: "Get comfortable with the fundamentals",
+    status: "doing",
+  },
+  {
+    id: 3,
+    title: "Keep on Going 🏆",
+    description: "You're almost there",
+    status: "doing",
+  },
+
+  {
+    id: 11,
+    title: "Learn Data Structures and Algorithms 📚",
+    description:
+      "Study fundamental data structures and algorithms to solve coding problems efficiently",
+    status: "todo",
+  },
+  {
+    id: 12,
+    title: "Contribute to Open Source Projects 🌐",
+    description:
+      "Gain practical experience and collaborate with others in the software development community",
+    status: "done",
+  },
+  {
+    id: 13,
+    title: "Build Portfolio Projects 🛠️",
+    description:
+      "Create a portfolio showcasing your skills and projects to potential employers",
+    status: "done",
+  },
+];
+
+const todoColumn = document.getElementById("todo-col");
+const doingColumn = document.getElementById("doing-col");
+const doneColumn = document.getElementById("done-col");
+const modalOpen = document.getElementById("modal-cnt")
+const modalClose = document.getElementById("close-btn")
+const titleInput = document.getElementById("input-title")
+const descriptionInput = document.getElementById("input-description")
+const statusInput = document.getElementById("input-status")
+
+/**Creating the task elements**/
+function createTaskElement(task){
+    let displayTask = document.createElement("div"); //creates a new div element in memory
+    displayTask.textContent = task.title;
+    return displayTask; //returning it so we can use it outside the function.
+}
+
+/***Check the task's status and append the task element to the correct column***/
+function appendTask(displayTask, status){
+  if (status === "todo"){
+    todoColumn.appendChild(displayTask);
+  } else if(status === "doing"){
+     doingColumn.appendChild(displayTask);
+  } else if(status === "done"){
+     doneColumn.appendChild(displayTask);
+  }
+}
+
+/**Main task rendering loop**/
+for(const task of initialTasks){ //Loop through all tasks.
+  const divTask = createTaskElement(task);
+  appendTask(divTask, task.status)
+
+//click listener so the modal opens with the task's details when clicked.
+  divTask.addEventListener("click", () => {
+  modalOpen.classList.add("display-modal");
+  titleInput.value = task.title;
+  descriptionInput.value = task.description;
+  statusInput.value = task.status;
+})
+}
+  //click listener so the modal closes.
+  modalClose.addEventListener("click", () => {
+    modalOpen.classList.remove("display-modal")
+  })
