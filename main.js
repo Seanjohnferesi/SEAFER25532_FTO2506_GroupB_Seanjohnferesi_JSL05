38a3e71c702dd24f0e919d6f3b881c132776d571
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-09-21 16:13:39 +0200

    Modularize code: separate responsibilities into storage, rendering,dom elements,  and modal management

--- main.js
+++ main.js
@@ -1,135 +1,59 @@
-let initialTasks = [
-  {
-    id: 1,
-    title: "Launch Epic Career 🚀",
-    description: "Create a killer Resume",
-    status: "todo",
-  },
-  {
-    id: 2,
-    title: "Master JavaScript 💛",
-    description: "Get comfortable with the fundamentals",
-    status: "doing",
-  },
-  {
-    id: 3,
-    title: "Keep on Going 🏆",
-    description: "You're almost there",
-    status: "doing",
-  },
 
-  {
-    id: 4,
-    title: "Learn Data Structures and Algorithms 📚",
-    description:
-      "Study fundamental data structures and algorithms to solve coding problems efficiently",
-    status: "todo",
-  },
-  {
-    id: 5,
-    title: "Contribute to Open Source Projects 🌐",
-    description:
-      "Gain practical experience and collaborate with others in the software development community",
-    status: "done",
-  },
-  {
-    id: 6,
-    title: "Build Portfolio Projects 🛠️",
-    description:
-      "Create a portfolio showcasing your skills and projects to potential employers",
-    status: "done",
-  },
-];
+import {initialTasks } from "./initialData.js";
+import { loadTasks, storeTasks} from "./localStorage.js";
+import { renderTasks, createTaskElement, appendTask } from "./taskRender.js";
+import { modalListenerOpen, modalListenerClose, modalTaskListener } from "./modalManager.js";
+import { 
+    modalOpen, modalClose, addTaskBtn, addTaskModal, mobileTaskBtn, closeTaskModal,
+    createTask, titleInput2, descriptionInput2, statusInput2 
+} from "./dom.js";
 
-const todoColumn = document.getElementById("todo-col");
-const doingColumn = document.getElementById("doing-col");
-const doneColumn = document.getElementById("done-col");
-const modalOpen = document.getElementById("modal-cnt")
-const modalClose = document.getElementById("close-btn")
-const titleInput = document.getElementById("input-title")
-const descriptionInput = document.getElementById("input-description")
-const statusInput = document.getElementById("input-status")
+let tasks = loadTasks();
 
-/**Add Task Modal**/
-const addTaskBtn = document.getElementById("add-task-btn")
-const addTaskModal = document.getElementById("modal-cnt-2")
-const closeTaskModal = document.getElementById("close-btn-2")
-const createTask = document.getElementById("create-task")
-let titleInput2 = document.getElementById("input-title-2")
-let descriptionInput2 = document.getElementById("input-description-2")
-let statusInput2 = document.getElementById("input-status-2")
+if (!tasks.length){
+  tasks = initialTasks;
+  storeTasks(tasks);
+};
 
-/**Creating the task elements**/
-function createTaskElement(task){
-    let displayTask = document.createElement("div"); //creates a new div element in memory
-    displayTask.textContent = task.title;
-    return displayTask; //returning it so we can use it outside the function.
+renderTasks(tasks);
+
+function clearTextField() {
+  titleInput2.value = "";
+  descriptionInput2.value = "";
 }
-
-/***Check the task's status and append the task element to the correct column***/
-function appendTask(displayTask, status){
-  if (status === "todo"){
-    todoColumn.appendChild(displayTask);
-  } else if(status === "doing"){
-     doingColumn.appendChild(displayTask);
-  } else if(status === "done"){
-     doneColumn.appendChild(displayTask);
-  }
-}
-
-
-//click listener so the modal closes.
-modalClose.addEventListener("click", () => {
-  modalOpen.classList.remove("display-modal")
-})
-
-//click listener so the add task modal open.
-addTaskBtn.addEventListener("click", () => {
-  addTaskModal.classList.add("display-modal")
-})
-
-//click listener so the modal closes.
-closeTaskModal.addEventListener("click", () => {
-  addTaskModal.classList.remove("display-modal")
-})
-
-let tasks = localStorage.getItem("initialTasks");
-
-if (!tasks){
-  localStorage.setItem("initialTasks", JSON.stringify(initialTasks));
-} else {
-    tasks = JSON.parse(localStorage.getItem("initialTasks"))
-
-    for (const task of tasks){
-      const divTask = createTaskElement(task);
-      appendTask(divTask, task.status)
-
-      //click listener so the modal opens with the task's details when clicked.
-      divTask.addEventListener("click", () => {
-        modalOpen.classList.add("display-modal");
-        titleInput.value = task.title;
-        descriptionInput.value = task.description;
-        statusInput.value = task.status;
-      })
-    }
-  }
 
 createTask.addEventListener("click", () => {
   let id = tasks.length + 1;
-  const newTasks = {
+  const newTask = {
     id: id,
     title: titleInput2.value,
     description: descriptionInput2.value,
     status: statusInput2.value
   }
-  tasks.push(newTasks)
-  localStorage.setItem("initialTasks", JSON.stringify(tasks));
-  const displayTask = createTaskElement(newTasks)
-  appendTask(displayTask, newTasks.status)
+  tasks.push(newTask)
+  storeTasks(tasks);
 
-  titleInput2.value = "";
-  descriptionInput2.value = "";
+  const displayTask = createTaskElement(newTask)
+  appendTask(displayTask, newTask.status)
+  modalTaskListener(displayTask, newTask);
+
+  clearTextField();
 })
+
+//click listener so the Task modal closes.
+modalListenerClose(modalClose, modalOpen)
+
+//click listener so the ADD TASK modal open.
+modalListenerOpen(addTaskBtn, addTaskModal)
+
+//click listener so the ADD TASK modal closes.
+modalListenerClose(closeTaskModal, addTaskModal)
+
+//click listener so the ADD TASK modal open for mobile.
+modalListenerOpen(mobileTaskBtn, addTaskModal)
+
+
+
 
 
 
